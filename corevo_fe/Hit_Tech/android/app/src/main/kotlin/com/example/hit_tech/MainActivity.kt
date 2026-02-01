e91fad96865e42c783801df91b72603105a8c56c
khuong1742005 <boyzsno1@gmail.com>
2026-02-01 15:30:50 +0700

    chore: remove flutter version

--- corevo_fe/Hit_Tech/android/app/src/main/kotlin/com/example/hit_tech/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.hit_tech
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() { }
--- corevo_fe/Hit_Tech/lib/model/request/auth/oauth2_google_request.dart
+++
@@ -1,9 +0,0 @@
-class Oauth2GoogleRequest {
-  final String idToken;
-
-  Oauth2GoogleRequest({required this.idToken});
-
-  Map<String, dynamic> toJson() {
-    return {'idToken': idToken};
-  }
-}
