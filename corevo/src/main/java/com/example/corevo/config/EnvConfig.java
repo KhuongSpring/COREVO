9abf5d7d28b0b377b02c49c30d1753ff05cedd82
khuong1742005 <boyzsno1@gmail.com>
2026-02-04 15:19:45 +0700

    feat: implement redis to cache otp

--- corevo/src/main/java/com/example/corevo/config/EnvConfig.java
+++ corevo/src/main/java/com/example/corevo/config/EnvConfig.java
@@ -6,7 +6,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration(proxyBeanMethods = false)
 public class EnvConfig {
     static {
-        Dotenv dotenv = Dotenv.load();
+        Dotenv dotenv = Dotenv.configure()
+                .directory("./")
+                .ignoreIfMissing()
+                .load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
     }
 }
---
+++ corevo/src/main/java/com/example/corevo/domain/dto/request/auth/otp/OtpRedisData.java
@@ -0,0 +1,17 @@
+package com.example.corevo.domain.dto.request.auth.otp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OtpRedisData implements Serializable {
+    private String otp;
+    private String data;
+}
